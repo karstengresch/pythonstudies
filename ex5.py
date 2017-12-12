3bc864947f3118629a4d99d511640fcea8cb5e17
Karsten Gresch <git-2015@by-doing.org>
2017-12-12 22:40:31 +0100

    Starting w/ base syntax

---
+++ ex5.py
@@ -0,0 +1,12 @@
+my_name = "Humpty Dumpty"
+my_age = 135
+my_height = 30
+my_weight = 30
+my_eyes = "Brown"
+my_teeth = "Brown"
+my_hair = "White"
+
+print(f"Let's talk about {my_name}!")
+print(f"He's about {my_weight} grams heavy.")
+total = my_age + my_height + my_weight
+print(f"If we add {my_age} and {my_height} and {my_weight}, we get {total}.")
