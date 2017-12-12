f6e842721699a50775934bdd68699e5c43a00c19
Karsten Gresch <git-2015@by-doing.org>
2017-12-12 21:13:37 +0100

    Starting w/ base syntax

--- ex3.py
+++ ex3.py
@@ -1,3 +1,9 @@
 # From https://www.safaribooksonline.com/library/view/learn-python-3
 print("I will now count the chickens")
-print("Hens", 25+30/6)
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100-25 * 3 % 4)
+print("To the eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 < 5 - 7)
+print("3 + 2 is:", 3 + 2)
+print("5-7 is: ", 5 - 7)
