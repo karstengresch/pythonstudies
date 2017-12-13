ff3ead2be3b43a08ce830d14f3f5c74be941c035
Karsten Gresch <git-2015@by-doing.org>
2017-12-13 23:13:02 +0100

    Changed as per 'challenge'

--- ex5.py
+++ ex5.py
@@ -1,12 +1,12 @@
-my_name = "Humpty Dumpty"
-my_age = 135
-my_height = 30
-my_weight = 30
-my_eyes = "Brown"
-my_teeth = "Brown"
-my_hair = "White"
+its_name = "Humpty Dumpty"
+its_age = 135
+its_height = 30
+its_weight = 30
+its_eyes = "Brown"
+its_teeth = "Brown"
+its_hair = "White"
 
-print(f"Let's talk about {my_name}!")
-print(f"He's about {my_weight} grams heavy.")
-total = my_age + my_height + my_weight
-print(f"If we add {my_age} and {my_height} and {my_weight}, we get {total}.")
+print(f"Let's talk about {its_name}!")
+print(f"He's about {its_weight} grams heavy.")
+total = its_age + its_height + its_weight
+print(f"If we add {its_age} and {its_height} and {its_weight}, we get {total}.")
